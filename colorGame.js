4baad6513bbd4b26462fa52903742cb7de11da6f
Drew Yuri <drewyuri@Drews-MacBook-Air.local>
2019-11-21 16:00:29 -0800

    adds eventlistener with alerts for clicked colors

--- colorGame.js
+++ colorGame.js
@@ -7,7 +7,19 @@ let colors = [
     "rgb(255, 0, 255)",
 ]
 let squares = document.querySelectorAll(".square");
+let pickedColor = colors[3];
+let colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textConent = pickedColor;
 
 for(let i = 0; i < squares.length; i++){
-    squares[i].style.backgroundColor = colors[i]
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].addEventListener("click",  function(){
+    let clickedColor = this.style.backgroundColor;
+        if(clickedColor === pickedColor){
+            alert("Correct")
+        } else {
+            alert("WRONG");
+        }
+    });
 }
